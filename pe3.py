8b110f6f3b34337f1b18a7528c5f581660bda7ab
allegra <agf.allegra@gmail.com>
2025-09-24 22:37:25 -0700

    Part B done \(so fast\!\)

--- pe3.py
+++ pe3.py
@@ -25,7 +25,36 @@ def encode(input_text,shift):
 
     print((alphabet,shifted_text))
 
-encode ("a", 3) # should return ([" a" , "b" , ... "z"] , "d")
-encode (" abc", 4 ) # should return ([" a" , "b" , ... "z"] , " efg ")
-encode (" xyz", 3 ) # should return ([" a" , "b" , ... "z"] , " abc ")
-encode ("j!K,2?", 3) # should return ([" a" , "b" , ... "z"] , "m!n,2 ?")
+#encode ("a", 3) # should return ([" a" , "b" , ... "z"] , "d")
+#encode (" abc", 4 ) # should return ([" a" , "b" , ... "z"] , " efg ")
+#encode (" xyz", 3 ) # should return ([" a" , "b" , ... "z"] , " abc ")
+#encode ("j!K,2?", 3) # should return ([" a" , "b" , ... "z"] , "m!n,2 ?")
+
+# PART B
+def decode(input_text,shift):
+    shifted_text = ''
+
+    for letter in list(input_text):
+
+        if letter in alphabet:
+
+            letter_index = alphabet.index(letter)
+            shift_index = letter_index - shift
+            
+            if shift_index < len(alphabet) and shift_index > 0:
+                new_letter = alphabet[shift_index]
+            else:
+                new_index = (letter_index - shift) % len(alphabet)
+                new_letter = alphabet[new_index]
+
+            shifted_text += new_letter
+       
+        else:
+            shifted_text = shifted_text + letter
+
+    print((alphabet,shifted_text))
+
+decode ("d", 3) # should return "a"
+decode (" efg", 4 ) # should return " abc "
+decode (" abc", 3 ) # should return " xyz "
+decode ("m!n,2?", 3) # should return "j!K,2 ?"
