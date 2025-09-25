d05195a4b267d42b59cab8e3dfea368216411e18
allegra <agf.allegra@gmail.com>
2025-09-25 00:30:37 -0700

    FOUR ERRORS and all in the banking section. woooooo

--- pe3.py
+++ pe3.py
@@ -12,11 +12,13 @@ def encode(input_text,shift):
     for letter in list(input_text.lower()):
 
         alphabet = lower_alphabet
-        
-        letter_index = alphabet.index(letter)
-        shift_index = (letter_index + shift) % len(alphabet)
-        new_letter = alphabet[shift_index]
-        shifted_text += new_letter
+        if letter in lower_alphabet:
+            letter_index = alphabet.index(letter)
+            shift_index = (letter_index + shift) % len(alphabet)
+            new_letter = alphabet[shift_index]
+            shifted_text += new_letter
+        else:
+            shifted_text += letter
 
     #print((lower_alphabet,shifted_text))
     return (lower_alphabet,shifted_text)
@@ -31,17 +33,18 @@ def decode(input_text,shift):
 
     for letter in list(input_text):
 
-        if letter in lower_alphabet:
-            alphabet = lower_alphabet
-        elif letter in upper_alphabet:
-            alphabet = upper_alphabet
+        if letter.lower() in lower_alphabet:
+            if letter.isupper():
+                letter_index = upper_alphabet.index(letter)
+                shift_index = (letter_index - shift) % len(upper_alphabet)
+                new_letter = upper_alphabet[shift_index]
+            elif letter.islower:
+                letter_index = lower_alphabet.index(letter)
+                shift_index = (letter_index - shift) % len(lower_alphabet)
+                new_letter = lower_alphabet[shift_index]
         else:
-            shifted_text += letter
-            continue
-
-        letter_index = alphabet.index(letter)
-        shift_index = (letter_index - shift) % len(alphabet)
-        new_letter = alphabet[shift_index]
+            new_letter = letter
+            
         shifted_text += new_letter
 
     #print((lower_alphabet,shifted_text))
@@ -73,20 +76,20 @@ class BankAccount():
             raise Exception("You may not deposit negative amounts.")
         else:
             self.balance = self.balance + amount
-            f"Your updated account balance is ${self.balance}."
+            #f"Your updated account balance is ${self.balance}."
         return self.balance
     
     def withdraw(self,amount):
         self.balance = self.balance - amount
-        f"Your updated account balance is ${self.balance}."
+        #f"Your updated account balance is ${self.balance}."
         return self.balance
     
     def view_balance(self):
-        f"Your account balance is ${self.balance}."
+        #f"Your account balance is ${self.balance}."
         return self.balance
 
 class SavingsAccount(BankAccount):
-    def __init__(self):
+    def __init__(self,name,creation_date,balance):
         super().__init__()
 
     def withdraw(self,amount):
@@ -98,12 +101,12 @@ class SavingsAccount(BankAccount):
             raise Exception("Your account must be in existence for 180 days before making any withdrawals.")
         else:
             self.balance = new_balance 
-            f"Your updated account balance is ${self.balance}."
+            #f"Your updated account balance is ${self.balance}."
     
         return self.balance
 
 class CheckingAccount(BankAccount):
-    def __init__(self):
+    def __init__(self,name,creation_date,balance):
         super().__init__()
 
     def withdraw(self,amount):
@@ -111,9 +114,9 @@ class CheckingAccount(BankAccount):
 
         if new_balance < 0:
             self.balance = new_balance - 30
-            f"Your account is overdrafted. You have been charged an additional $30 fee."
+            #f"Your account is overdrafted. You have been charged an additional $30 fee."
         else:
             self.balance = new_balance 
         
-        f"Your updated account balance is ${self.balance}."
+        #f"Your updated account balance is ${self.balance}."
         return self.balance
