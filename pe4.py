7f4f01ef230b1835b897ad2b59a5d8d56a7ed7f3
allegra <agf.allegra@gmail.com>
2025-11-07 11:28:55 -0500

    part B working too

--- pe4.py
+++ pe4.py
@@ -1,21 +1,16 @@
 import wikipedia
 import time
+import concurrent.futures
 from time import perf_counter
+from concurrent.futures import ThreadPoolExecutor
 
-
+search_list = wikipedia.search("Generative Artificial Intelligence")
 
 #
 # PART A
 # 
 
-search_list = wikipedia.search("Generative Artificial Intelligence")
-
-#page = wikipedia.page(search_list[1],auto_suggest=False)
-#print(page.title)
-#print(page.references)
-#print(len(search_list))
-
-start_time = time.perf_counter()
+start_time_A = time.perf_counter()
 
 for i in range(0, len(search_list)-1):
     page = wikipedia.page(search_list[i],auto_suggest=False)
@@ -26,8 +21,29 @@ for i in range(0, len(search_list)-1):
         for j in range(0,len(ref)-1):
             f.write(f"{ref[j]} \n")
 
-end_time = time.perf_counter()
+end_time_A = time.perf_counter()
+tot_time_A = end_time_A - start_time_A
+print(f"Part A took {tot_time_A} seconds.")
 
-tot_time = end_time - start_time
+#
+# PART B
+# 
 
-print(f"Elapsed time: {tot_time} seconds")
+
+start_time_B = time.perf_counter()
+
+def wiki_dl_and_save(topic):
+    page = wikipedia.page(topic,auto_suggest=False)
+    title = page.title
+    ref = page.references
+
+    with open(f"{title}.txt", "w") as f:
+        for j in range(0,len(ref)-1):
+            f.write(f"{ref[j]} \n")
+
+with concurrent.futures.ThreadPoolExecutor(10) as executor:
+    executor.map(wiki_dl_and_save, search_list)
+
+end_time_B = time.perf_counter()
+tot_time_B = end_time_B - start_time_B
+print(f"Part B took {tot_time_B} seconds.")
