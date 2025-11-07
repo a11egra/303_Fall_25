7db30fd8eb2d382620003b9e49f7207f2c6922c8
allegra <agf.allegra@gmail.com>
2025-11-07 11:22:23 -0500

    part A complete and working

--- pe4.py
+++ pe4.py
@@ -1,11 +1,21 @@
 import wikipedia
+import time
+from time import perf_counter
+
+
+
+#
+# PART A
+# 
 
 search_list = wikipedia.search("Generative Artificial Intelligence")
 
 #page = wikipedia.page(search_list[1],auto_suggest=False)
 #print(page.title)
 #print(page.references)
-print(len(search_list))
+#print(len(search_list))
+
+start_time = time.perf_counter()
 
 for i in range(0, len(search_list)-1):
     page = wikipedia.page(search_list[i],auto_suggest=False)
@@ -15,3 +25,9 @@ for i in range(0, len(search_list)-1):
     with open(f"{title}.txt", "w") as f:
         for j in range(0,len(ref)-1):
             f.write(f"{ref[j]} \n")
+
+end_time = time.perf_counter()
+
+tot_time = end_time - start_time
+
+print(f"Elapsed time: {tot_time} seconds")
