24f1d2f30d502003880858c166d87b3d714db0c5
allegra <agf.allegra@gmail.com>
2025-11-07 11:18:40 -0500

    files exporting, just need to add timer

--- pe4.py
+++ pe4.py
@@ -5,11 +5,13 @@ search_list = wikipedia.search("Generative Artificial Intelligence")
 #page = wikipedia.page(search_list[1],auto_suggest=False)
 #print(page.title)
 #print(page.references)
+print(len(search_list))
 
-for i in (0, len(search_list)-1):
+for i in range(0, len(search_list)-1):
     page = wikipedia.page(search_list[i],auto_suggest=False)
     title = page.title
     ref = page.references
 
     with open(f"{title}.txt", "w") as f:
-        f.write(ref)
+        for j in range(0,len(ref)-1):
+            f.write(f"{ref[j]} \n")
